2efc664e82162b21556e55ecdb7da30338db4cdc
Renan Silva <renan.costas@hotmail.com>
2020-01-05 18:50:50 -0300

    desenvolvimento da tela de perfil

--- resources/views/pages/perfil/perfil.blade.php
+++ resources/views/pages/perfil/perfil.blade.php
@@ -8,5 +8,173 @@
           </div>
 @include('templates.navbar')
 {{$pagina = 'perfil'}}
-<!--colocar conteudo -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="circulo">
+                <div><img src="../assets/img/sidebar-1.jpg" alt=""></div>
+            </div>
+
+            <div class="col-md-9">
+
+            <div class="row formulario">
+                <div class="col-md-12 titulo_perfil">
+                    <strong>Informações Pessoais</strong>
+                </div>
+
+                <div class="col-md-2">
+                    <label>Nome:</label>
+                </div>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" id="nome" placeholder="NOME">
+                </div>
+
+                <div class="col-md-2">
+                <label>CPF:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cpf" placeholder="CPF">
+            </div>
+            <div class="col-md-2">
+                <label>CPF:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cpf" placeholder="CPF">
+            </div>
+            <div class="col-md-2">
+                <label>CPF:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cpf" placeholder="CPF">
+            </div>
+            <div class="col-md-2">
+                <label>CPF:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cpf" placeholder="CPF">
+            </div>
+            </div>
+
+            </div>
+        
+        
+
+
+        <!--<div class="row formulario">
+            <div class="col-md-12 titulo_perfil">
+                <strong>Informações Pessoais</strong>
+            </div>
+        </div>
+
+        <br>
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Nome:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="nome" placeholder="NOME">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>CPF:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cpf" placeholder="CPF">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>E-mail:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="email" class="form-control" id="email" placeholder="E-MAIL">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Celular:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="celular" placeholder="CELULAR">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Rua:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="RUA">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Complemento:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="COMPLEMENTO">
+            </div>
+        </div>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Cidade / Estado:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="CIDADE / ESTADO">
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row formulario">
+            <div class="col-md-12 titulo_perfil">
+                <strong>Endereço Profissional</strong>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Rua:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="RUA">
+            </div>
+        </div>
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Complemento:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="COMPLEMENTO">
+            </div>
+        </div>
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>Cidade / Estado:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="CIDADE / ESTADO">
+            </div>
+        </div>
+        <div class="row formulario">
+            <div class="col-md-2">
+                <label>CEP:</label>
+            </div>
+            <div class="col-md-9">
+                <input type="text" class="form-control" id="cidade" placeholder="CEP">
+            </div>
+        </div>-->
+        
+
+        </div>   
+    </div>
+</div>
 @endsection
